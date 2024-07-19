3cb542d0ac4277bbc770200acb1adb3edf1a29b9
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2024-07-19 11:52:10 +0530

    Create Smallest-window-in-a-string-containing-all-the-characters-of-another-string.cpp

---
+++ Smallest window in a string containing all the characters of another string/Smallest-window-in-a-string-containing-all-the-characters-of-another-string.cpp
@@ -0,0 +1,26 @@
+string smallestWindow (string s, string t)
+    {
+        // Your code here
+        if (s.length() < t.length()) return "-1";
+        
+        vector<int> map(128, 0);
+        int count = t.length();
+        int start = 0, minStart = 0, minLen = INT_MAX;
+        
+        for (char c : t) map[c]++;
+        
+        for (int end = 0; end < s.length(); end++) {
+            if (map[s[end]]-- > 0) count--;
+            
+            while (count == 0) {
+                if (end - start + 1 < minLen) {
+                    minStart = start;
+                    minLen = end - start + 1;
+                }
+                
+                if (map[s[start++]]++ == 0) count++;
+            }
+        }
+        
+        return minLen == INT_MAX ? "-1" : s.substr(minStart, minLen);
+    }
