c6511aa7e2ce9b09cc4d5375094ed4a709074b02
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2024-07-22 15:59:04 +0530

    Time: 189 ms (34.87%), Space: 152.7 MB (17.51%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,21 +1,19 @@
 class Solution {
-    using int2=pair<int, int>; //(nums[i], i)
 public:
-
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         int n=nums.size();
-        priority_queue<int2> pq;
-
-        vector<int> ans(n-k+1);
-        for (int i=0; i<k; i++)
-            pq.push({nums[i], i});
-        
-        ans[0]=pq.top().first;
-        for(int i=k; i<n; i++){
-            while(!pq.empty() && pq.top().second<=(i-k))
-                pq.pop(); //Pop up element not in the window
-            pq.push({nums[i], i});
-            ans[i-k+1]=pq.top().first;//Max element for this window
+        vector<int>ans;
+        priority_queue<pair<int,int>>pq;
+        for(int i=0;i<k;i++){
+            pq.push({nums[i],i});
+        }
+        ans.push_back(pq.top().first);
+        for(int i=k;i<n;i++){
+            pq.push({nums[i],i});
+            while(!pq.empty() && pq.top().second<=(i-k)){
+                pq.pop();
+            }
+            ans.push_back(pq.top().first);
         }
         return ans;
     }
